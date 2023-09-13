ddd2c2659e56be9f7567a2022d7978699fac5271
Josephine James <69029497+josephine-james@users.noreply.github.com>
2023-09-13 14:24:12 +0530

    added gitignore

--- eventDjango/eventmgmtproject/eventmgmtproject/settings.py
+++ eventDjango/eventmgmtproject/eventmgmtproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=dvt6$1fvy$u5f9k*2dp#y!&c=!v34l1q_!3d*2j6w=-=qa_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
